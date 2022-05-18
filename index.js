293628c118bde74e221758999ab7e7df794aad25
AndrewMitchell25 <andrew.j.mitchell.247@gmail.com>
2022-05-18 14:36:56 -0700

    creating player['

--- index.js
+++ index.js
@@ -19,6 +19,43 @@ class Boundary{
     }
 }
 
+class Player{
+    constructor({position, velocity}){
+        this.position = position;
+        this.veloctiy = velocity;
+        this.radius = 15;
+    }
+
+    draw(){
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, 2*Math.PI);
+        c.fillStyle = 'yellow';
+        c.fill();
+        c.closePath();
+    }
+
+    update(){
+        this.draw();
+        this.position.x += this.veloctiy.x;
+        this.position.y += this.veloctiy.y;
+    }
+}
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
 const map = [
     ['-','-','-','-','-','-'],
     ['-',' ',' ',' ',' ','-'],
@@ -28,6 +65,7 @@ const map = [
 ]
 
 const boundaries = [];
+const player = new Player({position: {x: Boundary.width + Boundary.width/2, y: Boundary.height + Boundary.height/2}, velocity: {x: 0, y: 0}});
 
 map.forEach((row, i) => {
     row.forEach((symbol, j) => {
@@ -41,4 +79,28 @@ map.forEach((row, i) => {
     });
 });
 
-boundaries.forEach(boundary => {boundary.draw()})
+function animate(){
+    requestAnimationFrame(animate);
+    c.clearRect(0,0, pacCanvas.width, pacCanvas.height);
+    boundaries.forEach(boundary => {boundary.draw()})
+    player.update();
+}
+
+window.addEventListener('keydown', ({key}) => {
+    switch(key){
+        case 'w':
+            player.veloctiy.y = -5;
+            break;
+        case 'a':
+            player.veloctiy.x = -5;
+            break;
+        case 's':
+            player.veloctiy.y = 5;
+            break;
+        case 'd':
+            player.veloctiy.x = 5;
+            break;
+    }
+});
+
+animate();
